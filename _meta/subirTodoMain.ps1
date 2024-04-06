3c946f5e358241b02cddc5b9813c251c4b95e3df
Diego Freijo <giga.freijo@gmail.com>
2024-04-06 15:41:56 -0300

    detalles del nuevo script

--- _meta/subirTodoMain.ps1
+++ _meta/subirTodoMain.ps1
@@ -1,8 +1,6 @@
 git fetch publico
-git pull publico main
 git push publico main
 
 # Para que vuelva a estar alpha como default
-git fetch alpha 
-git pull alpha 
+git fetch alpha
 git push alpha main dev
