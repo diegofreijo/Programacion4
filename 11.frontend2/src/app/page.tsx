9b27a1d139d0b0bc24d7e7658222fc5db9f51fa0
Diego Freijo <giga.freijo@gmail.com>
2024-06-05 15:32:00 -0300

    - clase 11: duplique la clase 10

---
+++ 11.frontend2/src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { Listado } from "./Modelo";
+import api from "./utils";
+import Ciudad from "@/components/Ciudad";
+
+
+const LISTADO_INICIAL: Listado = { ciudades: [] };
+
+export default function Home() {
+  const [listado, setListado] = useState<Listado>(LISTADO_INICIAL);
+
+  useEffect(() => {
+    api<Listado>('/v1/listado')
+      .then((data: Listado) => {
+        setListado(data);
+      })
+  }, []);
+
+
+  return (
+    <>
+      <h1 className="text-3xl mb-8">Listado</h1>
+      <div className="grid flex 
+        lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 
+        gap-8 p-4">
+        {listado.ciudades.map(c =>
+          <Ciudad key={c.id} ciudad={c}></Ciudad>
+        )}
+      </div>
+    </>
+  );
+}
